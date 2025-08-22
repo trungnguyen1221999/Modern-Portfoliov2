a261acea6bc409c68dbb89ca0ecad693ef5e89d6
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-08-22 14:13:50 +0300

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // <-- dùng ./ để build tĩnh chạy được trên Vercel hoặc GH Pages
+  base: "/", // <-- dùng ./ để build tĩnh chạy được trên Vercel hoặc GH Pages
 });
